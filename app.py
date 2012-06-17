660ecf034e4f0417dffd9bb3c83d1eb631f00e26
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-17 16:14:45 +0100

    Add initial files.

---
+++ app.py
@@ -0,0 +1,21 @@
+__author__ = 'robyoung'
+
+import flask as fl
+import os
+
+def get_urls():
+    return [s.strip() for s in open(os.path.join(os.path.dirname(__file__), "urls.txt")).readlines()]
+
+app = fl.Flask(__name__)
+
+@app.route("/")
+def index():
+    return fl.redirect(fl.url_for("week_notes", date="2012-06-17"))
+
+
+@app.route("/notes/<date>")
+def week_notes(date):
+    return fl.render_template("%s.html" % date, urls=get_urls())
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
